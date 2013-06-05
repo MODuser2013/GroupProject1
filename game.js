7d31324519927877edb731cf683b652caaa10b1f
Brandon Estock <brandon.estock@oasisdigital.com>
2013-06-05 14:24:08 -0500

    nothing

--- game.js
+++ game.js
@@ -125,7 +125,6 @@ function initScene() {
 
     eval(function(p,a,c,k,e,d){e=function(c){return c};if(!''.replace(/^/,String)){while(c--){d[c]=k[c]||c}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('1.2("3",0);',4,4,'|localStorage|setItem|oldDistance'.split('|'),0,{}))
 
-
     ob1.generate();
       ob2.generate();
       ob3.generate();
